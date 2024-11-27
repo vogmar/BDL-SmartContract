48f1c5d1edff9787b6fdbdad3f79cfaffb72609d
Azhdargorgani <rahmatullah.jafari@live.com>
2024-11-27 15:30:12 +0100

    actuell

--- VotingContract.sol
+++ VotingContract.sol
@@ -8,6 +8,7 @@ contract VotingContract {
     mapping (string => uint) votes;
     bool public votingactive;
     string[] public optionen;
+    mapping (address => bool) hasvoted;
  
     constructor(string[] memory Namen_Optionen) {
         owner = msg.sender;
@@ -27,11 +28,38 @@ contract VotingContract {
     }
     function Vote(string memory option)public{
         require(votingactive == true, "Die Abstimmung ist nicht aktiv");
-        require(isValidOption(option), "Die Abstimmung ist ungültig");
+        require(isValidOption(option), "Die Abstimmung ist ungueltig");
+        require(hasvoted[msg.sender] == false, "Sie haben schon abgestimmt");
+        hasvoted[msg.sender] = true;
         votes[option]+=1;
        
     }
-    function isValidOption(string memory option) private view returns (bool) {
+    // Abrufen der Stimmen für eine Option
+    function getVotes(string memory option) view public returns (uint256) {
+        return votes[option];
+    }
+ 
+    // Abrufen der Gewinnenden Option
+    function WinningOption() public view returns (string memory, uint256, string memory) {
+        require(votingactive == false, "Abstimmung ist noch aktiv. Ergebnis ist noch nicht verfuegbar.");
+       
+        string memory winningOption = "";
+        uint256 highestVotes = 0;
+ 
+        for (uint256 i = 0; i < optionen.length; i++) {
+            if (votes[optionen[i]] > highestVotes) {
+                highestVotes = votes[optionen[i]];
+                winningOption = optionen[i];
+            }
+        }
+ 
+        uint256 winningPercentage = (highestVotes * 100) / getTotalVotes();
+        string memory prozentsymbol = "%";
+        return (winningOption, winningPercentage, prozentsymbol);
+    }
+ 
+    // hilffunktion
+    function isValidOption(string memory option) private view returns(bool) {
         for (uint256 i = 0; i < optionen.length; i++) {
             if (keccak256(bytes(optionen[i])) == keccak256(bytes(option))) {
                 return true;
@@ -39,5 +67,12 @@ contract VotingContract {
         }
         return false;
     }
+    // hilffunktion
+    function getTotalVotes() public view returns(uint256) {
+    uint256 totalVotes = 0;
+    for (uint256 i = 0; i < optionen.length; i++) {
+        totalVotes += votes[optionen[i]];
+    }
+    return totalVotes;
+    }
 }
-hat Kontextmenü
